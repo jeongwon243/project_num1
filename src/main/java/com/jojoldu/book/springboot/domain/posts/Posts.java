14de44527c7c43c3e1ff97ae3fa9d1df4dbe5313
jeongwon <cwjg1004@naver.com>
2022-06-21 21:03:44 +0900

    브랜치 테스트 커밋222윤자씨22222

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -29,7 +29,7 @@ public class Posts extends BaseTimeEntity {
 
     @Builder
     /*빌더 패턴 클래스*/
-    /*garden branch*/
+    /*garden branch윤자씨*/
     public Posts(String title, String content, String author){
         this.title = title;
         this.content = content;
