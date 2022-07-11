68e3a929de771bfd3c2d4aa4ddf0d292c85d2b1f
jeongwon <cwjg1004@naver.com>
2022-07-11 19:03:24 +0900

    브랜치 테스트 커밋222윤자씨22222바보추가

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -9,14 +9,14 @@ import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
-/*기본생성자 자동추가*/
+/*기본생성자 자동추가 매개변수가 없는 생성자를 자동생성. 일부 필수 필드를 빠뜨린채 생성하는 일 방지*/
 @Entity
 /*Entity 테이블과 링크될 클래스*/
 public class Posts extends BaseTimeEntity {
     @Id
     /*Id pk필드*/
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    /* pk생성규칙*/
+    /* pk생성규칙 IDENTITY방식 - AUTO_INCREMENT를 사용해 DB에서 자동으로IP할당하는 방식*/
     private Long id;
 
     @Column(length = 500, nullable = false)
